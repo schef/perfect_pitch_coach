79981e9a50bbb9a53e9ab69a33bea183ceee7be2
schef <zvanstefan@gmail.com>
2019-05-06 07:24:14 +0200

    added piano sounds

--- src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MainActivity.kt
+++ src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,6 +39,4 @@ class MainActivity : AppCompatActivity() {
             tvLog.setText(midiString)
         }
     }
-
-
 }
---
+++ src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MidiPlayer.kt
@@ -0,0 +1,11 @@
+package com.example.perfectpitchcoach
+
+import android.content.Context
+import android.media.MediaPlayer
+
+class MidiPlayer {
+    val mp = MediaPlayer()
+
+    fun noteOn() {
+    }
+}
