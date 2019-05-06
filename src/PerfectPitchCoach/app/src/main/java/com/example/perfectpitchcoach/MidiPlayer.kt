028f72e0a342f443214746204841fd748b2ca233
schef <zvanstefan@gmail.com>
2019-05-07 01:44:34 +0200

    soundpool as default

--- src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MidiPlayer.kt
+++ src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MidiPlayer.kt
@@ -1,19 +1,36 @@
 package com.example.perfectpitchcoach
 
-import android.content.Context
-import android.media.MediaPlayer
+import android.media.AudioAttributes
+import android.media.SoundPool
+import android.media.audiofx.LoudnessEnhancer
+import android.media.audiofx.PresetReverb
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.util.Log
+import android.widget.Toast
 import java.util.*
 import kotlin.concurrent.schedule
 
 class MidiPlayer {
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     companion object {
         val noteDuration = 1000L
         val firstTone = 21
         val lastTone = 108
-        var players = arrayListOf<MediaPlayer>()
+        var streamId = mutableMapOf<Int, Int>()
 
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        var attributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        val soundPool = SoundPool.Builder()
+            .setAudioAttributes(attributes)
+            .setMaxStreams(5)
+            .build();
 
         fun getResNote(midi: Int): Int {
             return App.ref.resources.getIdentifier("piano_" + midi, "raw", App.ref.packageName)
@@ -21,19 +38,28 @@ class MidiPlayer {
 
         init {
             for (x in firstTone..lastTone) {
-                players.add(MediaPlayer.create(App.ref, getResNote(x)))
+                soundPool.load(App.ref, getResNote(x), 1)
             }
+            soundPool.setOnLoadCompleteListener(SoundPool.OnLoadCompleteListener { soundPool, sampleId, status ->
+//                Toast.makeText(
+//                    App.ref,
+//                    "Sound load finished",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+                Log.e("MidiPlayer", soundPool.toString() + sampleId.toString() + status.toString())
+            })
         }
 
         fun noteOn(midi: Int) {
             Log.e("MidiPlayer", "noteOn: " + midi.toString())
-            players[midi - firstTone].start()
+            val id = soundPool.play(midi - firstTone, 1F, 1F, 1, 0, 1F)
+            streamId.put(midi, id)
         }
 
         fun noteOff(midi: Int) {
             Log.e("MidiPlayer", "noteOff: " + midi.toString())
-            players[midi - firstTone].pause()
-            players[midi - firstTone].seekTo(0)
+            soundPool.stop(streamId[midi]!!)
+            streamId.remove(midi)
         }
 
         fun playMultipleNotesMelodicly(pitchList: List<String>) {
@@ -49,7 +75,7 @@ class MidiPlayer {
             }
         }
 
-        fun playMultipleNotesHarmonicly(pitchList: List<String>){
+        fun playMultipleNotesHarmonicly(pitchList: List<String>) {
             for (pitch in pitchList) {
                 val midi = PitchParser.getMidiFromPitch(pitch)
                 noteOn(midi)
