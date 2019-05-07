a392f3c58761afbcd92c56206f5a0756c3062eb7
schef <zvanstefan@gmail.com>
2019-05-08 00:07:52 +0200

    commodore theme works

--- src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MainActivity.kt
+++ src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.perfectpitchcoach
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.WindowManager
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,8 +18,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val tvLog = findViewById<TextView>(R.id.tvLog)
         val etLog = findViewById<EditText>(R.id.etLog)
+        val etLogTv = findViewById<TextView>(R.id.etLogTv)
         val btnMidiToPitch = findViewById<TextView>(R.id.btnMidiToPitch)
         val btnPitchToMidi = findViewById<TextView>(R.id.btnPitchToMidi)
         val btnPlayNotesMelodicly = findViewById<TextView>(R.id.btnPlayNotesMelodicly)
@@ -60,5 +70,16 @@ class MainActivity : AppCompatActivity() {
                 MidiPlayer.playMultipleNotesHarmonicly(pitchList)
             }
         }
+
+        etLog.addTextChangedListener(object : TextWatcher {
+
+            override fun onTextChanged(arg0: CharSequence, arg1: Int, arg2: Int, arg3: Int) {}
+
+            override fun beforeTextChanged(arg0: CharSequence, arg1: Int, arg2: Int, arg3: Int) {}
+
+            override fun afterTextChanged(et: Editable) {
+                etLogTv.setText(et)
+            }
+        })
     }
 }
--- src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MidiPlayer.kt
+++ src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MidiPlayer.kt
@@ -11,79 +11,84 @@ import android.widget.Toast
 import java.util.*
 import kotlin.concurrent.schedule
 
-object MidiPlayer {
+class MidiPlayer {
 
-    val noteDuration = 1000L
-    val firstTone = 21
-    val lastTone = 108
-    var streamId = mutableMapOf<Int, Int>()
-    var loadedSoundCount = 0
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    companion object {
+        val noteDuration = 1000L
+        val firstTone = 21
+        val lastTone = 108
+        var streamId = mutableMapOf<Int, Int>()
+        var loadedSoundCount = 0
 
-    val attributes = AudioAttributes.Builder()
-        .setUsage(AudioAttributes.USAGE_MEDIA)
-        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-        .build()
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        var attributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
 
-    val soundPool = SoundPool.Builder()
-        .setAudioAttributes(attributes)
-        .setMaxStreams(7)
-        .build();
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        val soundPool = SoundPool.Builder()
+            .setAudioAttributes(attributes)
+            .setMaxStreams(7)
+            .build();
 
-    fun getResNote(midi: Int): Int {
-        return App.ref.resources.getIdentifier("piano_" + midi, "raw", App.ref.packageName)
-    }
-
-    init {
-        for (x in firstTone..lastTone) {
-            soundPool.load(App.ref, getResNote(x), 1)
+        fun getResNote(midi: Int): Int {
+            return App.ref.resources.getIdentifier("piano_" + midi, "raw", App.ref.packageName)
         }
-        soundPool.setOnLoadCompleteListener(SoundPool.OnLoadCompleteListener { soundPool, sampleId, status ->
 
-            loadedSoundCount++
-            if (loadedSoundCount >= (lastTone - firstTone)) {
+        init {
+            for (x in firstTone..lastTone) {
+                soundPool.load(App.ref, getResNote(x), 1)
+            }
+            soundPool.setOnLoadCompleteListener(SoundPool.OnLoadCompleteListener { soundPool, sampleId, status ->
+
+                loadedSoundCount++
+                if (loadedSoundCount >= (lastTone - firstTone)){
                 Toast.makeText(
                     App.ref,
                     "Sound load finished",
                     Toast.LENGTH_SHORT
                 ).show()
-            }
-            Log.e("MidiPlayer", soundPool.toString() + sampleId.toString() + status.toString())
-        })
-    }
-
-    fun noteOn(midi: Int) {
-        Log.e("MidiPlayer", "noteOn: " + midi.toString())
-        val sndId = midi - firstTone + 1
-        val strId = soundPool.play(sndId, 1F, 1F, 1, 0, 1F)
-        streamId.put(sndId, strId)
-    }
-
-    fun noteOff(midi: Int) {
-        Log.e("MidiPlayer", "noteOff: " + midi.toString())
-        val sndId = midi - firstTone + 1
-        soundPool.stop(streamId[sndId]!!)
-        streamId.remove(sndId)
-    }
-
-    fun playMultipleNotesMelodicly(pitchList: List<String>) {
-        Log.e("MidiPlayer", pitchList.toString())
-        val pitch = pitchList.get(0)
-        val midi = PitchParser.getMidiFromPitch(pitch)
-        noteOn(midi)
-        Timer().schedule(noteDuration) {
-            noteOff(midi)
-            if (pitchList.lastIndex > 0) {
-                playMultipleNotesMelodicly(pitchList.subList(1, pitchList.lastIndex + 1))
-            }
+                }
+                Log.e("MidiPlayer", soundPool.toString() + sampleId.toString() + status.toString())
+            })
         }
-    }
 
-    fun playMultipleNotesHarmonicly(pitchList: List<String>) {
-        for (pitch in pitchList) {
+        fun noteOn(midi: Int) {
+            Log.e("MidiPlayer", "noteOn: " + midi.toString())
+            val sndId = midi - firstTone + 1
+            val strId = soundPool.play(sndId, 1F, 1F, 1, 0, 1F)
+            streamId.put(sndId, strId)
+        }
+
+        fun noteOff(midi: Int) {
+            Log.e("MidiPlayer", "noteOff: " + midi.toString())
+            val sndId = midi - firstTone + 1
+            soundPool.stop(streamId[sndId]!!)
+            streamId.remove(sndId)
+        }
+
+        fun playMultipleNotesMelodicly(pitchList: List<String>) {
+            Log.e("MidiPlayer", pitchList.toString())
+            val pitch = pitchList.get(0)
             val midi = PitchParser.getMidiFromPitch(pitch)
             noteOn(midi)
-            Timer().schedule((noteDuration * 2.0).toLong()) {
+            Timer().schedule(noteDuration) {
                 noteOff(midi)
+                if (pitchList.lastIndex > 0) {
+                    playMultipleNotesMelodicly(pitchList.subList(1, pitchList.lastIndex + 1))
+                }
+            }
+        }
+
+        fun playMultipleNotesHarmonicly(pitchList: List<String>) {
+            for (pitch in pitchList) {
+                val midi = PitchParser.getMidiFromPitch(pitch)
+                noteOn(midi)
+                Timer().schedule((noteDuration * 2.0).toLong()) {
+                    noteOff(midi)
+                }
             }
         }
     }
