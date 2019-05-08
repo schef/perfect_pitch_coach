34969fed16f4647396bfc6b4b4c7e0bfd5905a1e
schef <zvanstefan@gmail.com>
2019-05-08 17:17:01 +0200

    cleaned up gui

--- src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MainActivity.kt
+++ src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.text.Editable
+import android.text.InputFilter
 import android.text.TextWatcher
 import android.view.WindowManager
 
@@ -18,22 +19,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
         val tvLog = findViewById<TextView>(R.id.tvLog)
         val etLog = findViewById<EditText>(R.id.etLog)
-        val etLogTv = findViewById<TextView>(R.id.etLogTv)
+//        val etLogTv = findViewById<TextView>(R.id.etLogTv)
         val btnMidiToPitch = findViewById<TextView>(R.id.btnMidiToPitch)
         val btnPitchToMidi = findViewById<TextView>(R.id.btnPitchToMidi)
         val btnPlayNotesMelodicly = findViewById<TextView>(R.id.btnPlayNotesMelodicly)
         val btnPlayNotesHarmonicly = findViewById<TextView>(R.id.btnPlayNotesHarmonicly)
         MidiPlayer
 
+        etLog.filters = arrayOf(InputFilter.AllCaps())
+
         btnMidiToPitch?.setOnClickListener {
             //TODO: is there a oneliner?
             val midiList = arrayListOf<Int>()
-            val textList = etLog.text.split(" ")
+            val textList = etLog.text.toString().toLowerCase().filter { it in "cdefgahb" }.split(" ")
             for (text in textList) {
                 val number = text.toIntOrNull()
                 if (number != null) {
@@ -46,7 +46,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnPitchToMidi?.setOnClickListener {
-            val pitchList = etLog.text.split(" ")
+            val pitchList = etLog.text.toString().toLowerCase().split(" ")
             var tvString = ""
             if (PitchParser.isPitchListValid(pitchList)) {
                 val midiList = PitchParser.getMidiListFromPitchList(pitchList)
@@ -58,28 +58,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnPlayNotesMelodicly?.setOnClickListener {
-            val pitchList = etLog.getText().split(" ")
+            val pitchList = etLog.getText().toString().toLowerCase().split(" ")
             if (PitchParser.isPitchListValid(pitchList)) {
                 MidiPlayer.playMultipleNotesMelodicly(pitchList)
             }
         }
 
         btnPlayNotesHarmonicly?.setOnClickListener {
-            val pitchList = etLog.getText().split(" ")
+            val pitchList = etLog.getText().toString().toLowerCase().split(" ")
             if (PitchParser.isPitchListValid(pitchList)) {
                 MidiPlayer.playMultipleNotesHarmonicly(pitchList)
             }
         }
-
-        etLog.addTextChangedListener(object : TextWatcher {
-
-            override fun onTextChanged(arg0: CharSequence, arg1: Int, arg2: Int, arg3: Int) {}
-
-            override fun beforeTextChanged(arg0: CharSequence, arg1: Int, arg2: Int, arg3: Int) {}
-
-            override fun afterTextChanged(et: Editable) {
-                etLogTv.setText(et)
-            }
-        })
     }
 }
