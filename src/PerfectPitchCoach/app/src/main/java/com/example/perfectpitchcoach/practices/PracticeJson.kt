a12058727a03e0a782104be0dbb23c4c899b7d89
schef <zvanstefan@gmail.com>
2019-05-08 20:50:53 +0200

    added practicejson

---
+++ src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/practices/PracticeJson.kt
@@ -0,0 +1,11 @@
+package com.example.perfectpitchcoach.practices
+
+class PracticeJson {
+    var group: String? = null
+    var practice: String? = null
+    var description: String? = null
+    var uuid: String? = null
+    var maxHits: Int = 0
+    var practiceType: String? = null
+    var practiceBatch : List<PracticeJsonBatch>? = null
+}
