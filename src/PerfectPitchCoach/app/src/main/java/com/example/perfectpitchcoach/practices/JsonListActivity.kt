2a3697aef7b26b4ca31839a40cb079e5b6c30855
schef <zvanstefan@gmail.com>
2019-05-14 08:19:18 +0200

    addded click listener

--- src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/practices/JsonListActivity.kt
+++ src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/practices/JsonListActivity.kt
@@ -2,6 +2,7 @@ package com.example.perfectpitchcoach.practices
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.example.perfectpitchcoach.R
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_json_list.*
@@ -9,11 +10,16 @@ import kotlinx.android.synthetic.main.activity_json_list.*
 
 class JsonListActivity : AppCompatActivity() {
 
-    val mobileArray = arrayOf("Android", "IPhone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X")
+    val mobileArray =
+        arrayOf("Android", "IPhone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.perfectpitchcoach.R.layout.activity_json_list)
+
+        val jsonList = ListEntity.getListFromJsonRaw(R.raw.masterclasses)
+
+//        Log.e("JsonListActivity", jsonList.toString())
 
         val adapter = ArrayAdapter(
             this,
@@ -21,5 +27,9 @@ class JsonListActivity : AppCompatActivity() {
         )
 
         lvList.setAdapter(adapter)
+
+        lvList.setOnItemClickListener { parent, view, position, id ->
+            Log.e("JsonListActivity", jsonList!!.list!!.get(position).toString())
+        }
     }
 }
