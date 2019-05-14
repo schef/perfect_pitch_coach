7ea4530b4d41a0f453171fabe7c773f9862e1878
schef <zvanstefan@gmail.com>
2019-05-14 20:05:28 +0200

    added recycler view

--- src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/practices/JsonListActivity.kt
+++ src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/practices/JsonListActivity.kt
@@ -2,17 +2,15 @@ package com.example.perfectpitchcoach.practices
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.example.perfectpitchcoach.R
-import android.widget.ArrayAdapter
+import com.example.perfectpitchcoach.practices.JsonListEntity.ListInfo
 import kotlinx.android.synthetic.main.activity_json_list.*
 
 //https://medium.com/@hinchman_amanda/working-with-recyclerview-in-android-kotlin-84a62aef94ec
 
 class JsonListActivity : AppCompatActivity() {
-
-    val mobileArray =
-        arrayOf("Android", "IPhone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,17 +18,12 @@ class JsonListActivity : AppCompatActivity() {
 
         val jsonList = JsonListEntity.getListFromJsonRaw(R.raw.masterclasses)
 
-//        Log.e("JsonListActivity", jsonList.toString())
+        tvTitle.setText(jsonList?.description)
 
-        val adapter = ArrayAdapter(
-            this,
-            com.example.perfectpitchcoach.R.layout.activity_json_list_item, mobileArray
-        )
+        Log.e("JsonListActivity", jsonList.toString())
 
-        lvList.setAdapter(adapter)
+        rvList.layoutManager = LinearLayoutManager(this)
+        rvList.adapter = JsonListAdapter(jsonList?.list)
 
-        lvList.setOnItemClickListener { parent, view, position, id ->
-            Log.e("JsonListActivity", jsonList!!.list!!.get(position).toString())
-        }
     }
 }
--- src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/practices/JsonListAdapter.kt
+++ src/PerfectPitchCoach/app/src/main/java/com/example/perfectpitchcoach/practices/JsonListAdapter.kt
@@ -1,35 +1,33 @@
 package com.example.perfectpitchcoach.practices
 
-import android.R
 import android.content.Context
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import com.example.perfectpitchcoach.App
+import com.example.perfectpitchcoach.R
+import kotlinx.android.synthetic.main.activity_json_list_item.view.*
 
 
-class JsonListAdapter(context: Context, items: ArrayList<JsonListEntity>) : ArrayAdapter<JsonListEntity>(context, 0, items) {
+class JsonListAdapter(val items: List<JsonListEntity.ListItem>?) :
+    RecyclerView.Adapter<ViewHolder>() {
 
-    @Override
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
-        var convertView = convertView
-
-        // Get the data item for this position
-
-        val item = getItem(position)
-
-        // Check if an existing view is being reused, otherwise inflate the view
-
-        if (convertView == null) {
-
-            convertView = LayoutInflater.from(context).inflate(R.layout.activity_list_item, parent, false)
-
-        }
-
-        // Return the completed view to render on screen
-
-        return convertView
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(App.ref).inflate(R.layout.activity_json_list_item, parent, false))
     }
 
+    override fun getItemCount(): Int {
+        return items!!.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvItemName.text = items!!.get(position).name
+        holder.tvItemPercent.text = 0.toString()
+    }
+}
+
+class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val tvItemName = view.tvItemName
+    val tvItemPercent = view.tvItemPercent
 }
